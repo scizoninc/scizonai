36c41187002bde81cd06a736701d9e67d6607191
Pedro <pedrodanielsg1020@gmail.com>
2026-01-04 12:28:14 -0300

    >

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"; 
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Seus imports de páginas
 import Landing from "./pages/Landing";
@@ -43,7 +42,6 @@ const App = () => (
         </Routes>
       </BrowserRouter>
       <Analytics />
-      <SpeedInsights />
     </TooltipProvider>
   </QueryClientProvider>
 );
