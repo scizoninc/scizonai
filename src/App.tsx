dd90a8d49c6d271b188af8cc0661ae86969f4ebd
Pedro <pedrodanielsg1020@gmail.com>
2026-01-04 12:23:35 -0300

    >

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
       <Analytics />
-      <SpeedInsights/>
+      <SpeedInsights />
     </TooltipProvider>
   </QueryClientProvider>
 );
